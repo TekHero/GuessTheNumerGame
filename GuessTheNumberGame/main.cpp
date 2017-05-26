3970923d2cdd126a68b5d6db1e2eed136de76f7b
Brian Lim <Brian@Brians-iMac.home>
2017-05-25 22:53:10 -0400

    First upload

--- GuessTheNumberGame/main.cpp
+++ GuessTheNumberGame/main.cpp
@@ -8,8 +8,72 @@
 
 #include <iostream>
 
+using namespace std;
+
+void introMsg();
+int generateRandNum();
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    srand(time(0));
+    
+    int randNum = generateRandNum();
+    int usersGuess;
+    char usersChoice;
+    
+    introMsg();
+    do {
+        
+        cout << "? ";
+        cin >> usersGuess;
+        
+        if (cin.fail()) {
+            
+            cout << "Invalid guess. Try again." << endl;
+            cin.clear();
+            cin.ignore(256, '\n');
+
+        } else {
+            
+            if (usersGuess < randNum) {
+                cout << "Too Low. Try again." << endl;
+            } else if (usersGuess > randNum) {
+                cout << "Too High. Try again." << endl;
+            } else if (usersGuess == randNum) {
+                cout << "\n";
+                cout << "Excellent! You guessed the number!" << endl;
+                cout << "Would you like to play again (y or n)? ";
+                
+                cin >> usersChoice;
+                cout << "\n";
+                
+                if (usersChoice == 'y' || usersChoice == 'Y') {
+                    randNum = generateRandNum();
+                    introMsg();
+                } else if (usersChoice == 'n' || usersChoice == 'N') {
+                    return 0;
+                } else if (usersChoice != 'n' || usersChoice != 'N' || usersChoice != 'y' || usersChoice != 'Y') {
+                    cout << "Invalid choice." << endl;
+                }
+            }
+
+        }
+        
+    } while (usersGuess != randNum);
+    
     return 0;
+}
+
+void introMsg() {
+    cout << "I have a number between 1 and 1000." << endl;
+    cout << "Can you guess my number?" << endl;
+    cout << "Please type your first guess." << endl;
+}
+
+int generateRandNum() {
+    
+    int number;
+    number = rand() % 1000 + 1;
+    cout << "Generated Number is: " << number << endl;
+    return number;
 }
